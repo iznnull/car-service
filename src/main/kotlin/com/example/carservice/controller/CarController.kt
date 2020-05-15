74fa994d4008acefc18e5381f078dc8b5d14276a
iespinob <iespinob@everis.com>
2020-05-15 12:20:57 +0200

    Use of extensions

--- src/main/kotlin/com/example/carservice/controller/CarController.kt
+++ src/main/kotlin/com/example/carservice/controller/CarController.kt
@@ -1,14 +1,19 @@
 package com.example.carservice.controller
 
 import com.example.carservice.dto.CarDto
-import com.example.carservice.mappers.CarMapper
+import com.example.carservice.mappers.toCarModel
 import com.example.carservice.model.Car
 import com.example.carservice.repository.CarRepository
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/cars")
-class CarController(private val carRepository: CarRepository, private val carMapper: CarMapper) {
+class CarController(private val carRepository: CarRepository) {
 
     @GetMapping()
     fun findAll(): Iterable<Car> = carRepository.findAll()
@@ -17,5 +22,5 @@ class CarController(private val carRepository: CarRepository, private val carMap
     fun findById(@PathVariable id: Long) = carRepository.findById(id)
 
     @PostMapping()
-    fun addCar(@RequestBody carDto: CarDto) = carRepository.save(carMapper.toCarModel(carDto))
+    fun addCar(@RequestBody carDto: CarDto) = carRepository.save(carDto.toCarModel())
 }
